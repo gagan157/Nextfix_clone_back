ef5a9903dbff39f9796af0d3f1dd422e9dd90bc4
gagan <gaganrehal93@gmail.com>
2023-08-04 17:04:26 +0530

    allow all cros

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const allowOrigins = [
+    '*',
     'http://localhost:5000',
     'http://localhost:3000',
     'https://netxflic-b-clone.onrender.com',
